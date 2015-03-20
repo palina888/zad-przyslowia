9fb9bb74782e582e5c0d7ee05b7ca3bb4d6cd2fb
Mateusz Charkot <mateusz.charkot@gmail.com>
2015-03-20 13:35:38 +0100

    Ciasno w glowie

---
+++ features/ciasno-w-glowie.feature
@@ -0,0 +1,5 @@
+Feature: Web pages
+
+  Scenario: Ciasno w głowie, przestronno w gębie.
+    Given I am on "ciasno-w-glowie.html"
+    Then I should see "Ciasno w głowie, przestronno w gębie."
--- src/AppBundle/Controller/SecondController.php
+++ src/AppBundle/Controller/SecondController.php
@@ -107,6 +107,13 @@ class SecondController extends Controller
     /*
      * Q
      */
+     /**
+     * @Route("/ciasno-w-glowie.html", name="ciasno-w-glowie")
+     */
+    public function ciasnowglowieAction()
+    {
+        return $this->render('default/ciasno-w-glowie.html.twig');
+    }
 
     /*
      * R
